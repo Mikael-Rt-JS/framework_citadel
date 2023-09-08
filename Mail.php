5b4f8f8686aec9923ad6e7ce762aaeae0604930f
Mikael-Rt-JS <61776022+Mikael-Rt-JS@users.noreply.github.com>
2023-09-09 03:22:49 +0400

    Update Mail.php
    добавление кода

--- Mail.php
+++ Mail.php
@@ -1,1 +1,27 @@
+<?php
 
+class Mail{
+    public function sendToEmail($to,$subject,$message,$headers){
+        mail($to,$subject,$message,$headers);
+    }
+    //потверждаем почту
+    public function mailConfirmation($post){
+        $name=$post['name'];
+        $surname=$post['surname'];
+        $to=$post['email'];
+        $subject="Новый аккаунта/New account";
+        $headers="From: urartubot.ru \r\n";
+        //$headers.="Reply-To: mikael.vue@gmail.com \r\n";
+        $headers.="X-Mailer: PHP/".phpversion()."\r\n";
+        $headers.="Content-Type: text/html; charset=utf-8 \r\n";
+        
+        $message='<!DOCTYPE html><html><body>
+<h3>Name: '.$name.' '.$surname.'</h3>
+<p>Пройдите по ссылке для потверждение почты! </br> Follow the link to confirm your email!</p>
+<a href="https://urartubot.ru/confirmation_mail.php">Ready</a>
+</body></html>';
+
+        $this->sendToEmail($to,$subject,$message,$headers);
+    }
+}
+?>
